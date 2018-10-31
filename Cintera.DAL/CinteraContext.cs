d68ba15e0adb55beb6fb0bc704572e4ccdca7b59
vn00m8d <vn00m8d@mx.wal-mart.com>
2018-10-31 17:02:37 -0600

    Se crea la base de datos

--- Cintera.DAL/CinteraContext.cs
+++ Cintera.DAL/CinteraContext.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration.Conventions;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 
 namespace Cintera.DAL
 {
@@ -14,25 +8,6 @@ namespace Cintera.DAL
             : base("CinteraContext")
         {
 
-        }
-
-        public DbSet<Case> Cases { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Conventions
-                .Remove<PluralizingTableNameConvention>();
-
-            modelBuilder.Entity<Sighting>()
-                .Property(x => x.Address)
-                .HasMaxLength(256)
-                .IsRequired();
-            modelBuilder.Entity<Vehicle>()
-                .Property(x => x.VehicleIdentificationNumber)
-                .IsRequired()
-                .HasMaxLength(17);
-
-            base.OnModelCreating(modelBuilder);
         }
     }
 }
