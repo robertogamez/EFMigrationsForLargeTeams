ca841f761dc13f25d31052084b5f16d109dbc45c
vn00m8d <vn00m8d@mx.wal-mart.com>
2018-10-31 13:32:08 -0600

    Primer commit
    Subiendo código fuente

---
+++ Cintera.DAL/CinteraContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cintera.DAL
+{
+    public class CinteraContext : DbContext
+    {
+        public CinteraContext()
+            : base("CinteraContext")
+        {
+
+        }
+
+        public DbSet<Case> Cases { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Sighting>()
+                .Property(x => x.Address)
+                .HasMaxLength(256)
+                .IsRequired();
+            modelBuilder.Entity<Vehicle>()
+                .Property(x => x.VehicleIdentificationNumber)
+                .IsRequired()
+                .HasMaxLength(17);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
